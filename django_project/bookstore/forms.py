1c779f2201539b1cdce807b891607bb621c484cf
eliBurstiner <51697436+eliBurstiner@users.noreply.github.com>
2021-02-26 17:47:07 -0500

    Edit Profile
    Added edit profile page, added confirm registration page, added text to indicate mandatory fields on registration page

--- django_project/bookstore/forms.py
+++ django_project/bookstore/forms.py
@@ -2,13 +2,13 @@ from django import forms
 from .models import User
 
 class RegistrationForm(forms.ModelForm):
-    username = forms.CharField(label="Username", max_length=20)
-    password = forms.CharField(label="Password", max_length=20,  widget=forms.PasswordInput)
-    email = forms.EmailField(label="Email")
-    phone = forms.CharField(label="Phone", max_length=10)
-    card_num = forms.CharField(label="Card number", max_length=16)
-    card_exp = forms.CharField(label="Expiration date", max_length=5)
-    card_code = forms.CharField(label="Security code", max_length=3)
+    username = forms.CharField(label="Username*", max_length=20)
+    password = forms.CharField(label="Password*", max_length=20,  widget=forms.PasswordInput)
+    email = forms.EmailField(label="Email*")
+    phone = forms.CharField(label="Phone*", max_length=10)
+    card_num = forms.CharField(label="Card number*", max_length=16)
+    card_exp = forms.CharField(label="Expiration date*", max_length=5)
+    card_code = forms.CharField(label="Security code*", max_length=3)
 
     class Meta:
         model = User

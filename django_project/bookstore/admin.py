c7276c1f418d0f061d2accf2db7718375c926a26
davidseeler <david.seeler@gmail.com>
2021-03-03 14:44:52 -0500

    Add real prices for all books

--- django_project/bookstore/admin.py
+++ django_project/bookstore/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Book, User
+from .models import Book
 
 admin.site.register(Book)
-admin.site.register(User)
--- django_project/bookstore/models.py
+++ django_project/bookstore/models.py
@@ -6,18 +6,7 @@ class Book(models.Model):
     description = models.TextField()
     image = models.CharField(max_length=500)
     year = models.CharField(max_length=4)
+    price = models.DecimalField(max_digits=4, decimal_places=2, default=10.00)
 
     def __str__(self):
         return self.title
-
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField()
-    phone = models.CharField(max_length=10)
-    card_num = models.CharField(max_length=16)
-    card_exp = models.CharField(max_length=5)
-    card_code = models.CharField(max_length=3)
-
-    def __str__(self):
-        return self.username

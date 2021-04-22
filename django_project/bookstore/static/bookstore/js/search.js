10a50c9f5599026c98fcc88e8a26eaec8f74baff
davidseeler <david.seeler@gmail.com>
2021-04-22 15:26:21 -0400

    Remove star function from search.js

--- django_project/bookstore/static/bookstore/js/search.js
+++ django_project/bookstore/static/bookstore/js/search.js
@@ -5,17 +5,3 @@ window.onload = (event) => {
 function reset(){
     location.reload();
 }
-
-function setStars(id, rating){
-    let stars = "";
-    for (let i = 0; i < 5; i++){
-        if (i < rating){
-            stars += "<span class='fa fa-star checked'></span>";
-        }
-        else{
-            stars += "<span class='fa fa-star'></span>";
-        }
-    }
-
-    document.getElementById("rating-" + id).innerHTML = stars;    
-}
